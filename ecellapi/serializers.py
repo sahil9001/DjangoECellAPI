38e194a9ced2f2c1ef50ab62cf893d83dfd3c7db
Sahil Silare <sahilsilare@gmail.com>
2020-03-12 13:25:54 +0530

    Added id field

--- ecellapi/serializers.py
+++ ecellapi/serializers.py
@@ -4,4 +4,4 @@ from .models import Event
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = ('name','venue','date','time','details','details_html','cover_pic','icon','email','flag','year','created_at','modified_at')
+        fields = ('id','name','venue','date','time','details','details_html','cover_pic','icon','email','flag','year','created_at','modified_at')
